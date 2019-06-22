aab91aaf3badc7606bb15ac4aadc6ee356ea1c29
ryan <2869888186@qq.com>
2019-06-22 10:45:42 +0800

    add actions.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import './App.css';
+import './actions'
+import {addTodo, setVisibilityFilter, toggleTodo} from "./actions";
 
 function App() {
+
+    console.dir(addTodo('a'))
+    console.dir(toggleTodo('1'))
+    console.dir(setVisibilityFilter('b'))
   return (
     <div className="App">
       right
